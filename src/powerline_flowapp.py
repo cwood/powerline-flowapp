5bbe76a32fb94068397b623903c9771786346df4
Colin Wood <cwood@atlassian.com>
2022-08-09 15:51:29 -0700

    Initial project for powerline status for flowapp

---
+++ src/powerline_flowapp.py
@@ -0,0 +1,32 @@
+from powerline.lib.shell import asrun
+
+
+def flowapp(pl):
+    status_delimiter = '-~`/='
+    ascript = '''
+        tell application "System Events"
+            set process_list to (name of every process)
+        end tell
+
+        if process_list contains "Flow" then
+            tell application "Flow"
+                set flow_status to "" & getTime & "{0}" & getPhase
+                return flow_status
+            end tell
+        else
+            return "not running"
+        end if
+    '''.format(status_delimiter)
+
+    flow = asrun(pl, ascript)
+    if not asrun:
+        return None
+
+    flow_time = flow.split(status_delimiter)
+
+    return [{
+        "contents": "{phase} | {time}".format(
+            phase=flow_time[1], time=flow_time[0]),
+        "highlight_groups": ["flowapp"],
+        "divider_highlight_group": "flowapp:divider",
+    }]
